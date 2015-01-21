a9289da133c6e029404d5d619bbef5131d1e2479
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2015-01-21 23:05:34 +0800

    updated server_names

--- attributes/nginx.rb
+++ attributes/nginx.rb
@@ -3,6 +3,6 @@ default[:rocketws][:nginx] = {
         :on     => false,
         :dir    => "/etc/nginx/ssl"
     },
-    :server_names => ["rocketws1.yourdomain.com", "rocketws2.yourdomain.com"],
+    :server_names => [],
     :proxy_read_timeout => 604800
 }
