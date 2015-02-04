8edd4b6ff76eb40540b8782f838be294fdc75364
Maksim Ekimovskii <ekimovsky.maksim@gmail.com>
2015-02-04 16:30:22 +0800

    Added nginx ssl dir creation if not

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -11,6 +11,16 @@ link "/etc/nginx/sites-enabled/#{node[:rocketws][:project]}.nginx.conf" do
     to "/etc/nginx/sites-available/#{node[:rocketws][:project]}.nginx.conf"
 end
 
+# Create ssl directory if not
+directory "/etc/nginx/ssl" do
+    owner	"root"
+    group   "root"
+    mode	644
+    action  :create
+    not_if  { File.exist?("/etc/nginx/ssl") }
+end
+
+
 service "nginx" do
     action :restart
 end
